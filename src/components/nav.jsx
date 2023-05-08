908e77cb3e68a087fb04f7196c2d5b80a0107b7c
hapistolero <ilhamhardanz2507@gmail.com>
2023-05-09 00:12:15 +0700

    commit landing page

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
+import Image from 'next/image'
+import styles from '../styles/nav.module.css'
 
 export default function Nav() {
+  const [ham, setHam] = useState(false)
   return (
-    <div></div>
+  <nav className={`${styles.navBar} d-flex p-2`}>
+    <div className='m-2' >
+    <Image className={styles.navLogo} alt='logo' src='logo.svg' width={50} height={50} />
+
+    </div>
+    <div className={`${styles.divNavText} col-xl-5 col-md-5 `}>
+    <h1 className={`${styles.navH1} container-sm fs-6 m-2 text-sm text-start text-white`}>HMPS Keperawatan Anestesiologi</h1>
+    <span className={`${styles.navP} fs-5 text-white`}>Universitas Muhammaddiyah Purwokerto</span>
+    </div>
+    <ul className={`${styles.ulNav} col-xl-5  col-md-5 text-white d-flex ${ !ham ? styles.show : ''}  `}>
+      <li className=' col-xl-5 text-white  pt-4 d-inline'>Home</li>
+      <li className=' col-xl-5 text-white  pt-4 d-inline'>About</li>
+      <li className=' col-xl-5 text-white  pt-4 d-inline'>Contact</li>
+    </ul>
+    <div onClick={()=> setHam(!ham)} className={`${styles.hamNav} text-white  d-flex flex-column `}>
+      <span className={`${ styles.an11 } bg-white w-100 m-1 p-0 ${ !ham ? styles.an1 : ''} `}></span>
+      <span className={`${ styles.an22 } bg-white w-100 m-1 p-0 ${ !ham ? styles.an2 : ''}`}></span>
+      <span className={`${ styles.an33 } bg-white w-100 m-1 p-0 ${ !ham ? styles.an3 : ''}`}></span>
+    </div>
+    
+  </nav>
   )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import Script from 'next/script';
 import Head from 'next/head';
+import '@/styles/nav.module.css'
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
